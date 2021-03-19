4eda3ebea3dbc6eb88c76e27836827d1124d13e2
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2021-03-19 17:18:17 +0100

    Added winning and loosing conditions.

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-  std::cout << "You are a secret agent breaking into a secure server room...";
+  std::cout << "You are a secret nixi agent breaking into a secure server room\nSecure by the most advance firewall in the the world the HAL9000";
   std::cout << std::endl;
   std::cout << "Enter the correct code to continue..." << std::endl
             << std::endl;
@@ -20,7 +20,7 @@ int main()
   std::cout << std::endl;
 
   int guessA, guessB, guessC;
-  std::cout << "Your input: (separate by space)" << std::endl;
+  std::cout << "Hack the sever by entring the magic code: ";
   std::cin >> guessA;
   std::cin >> guessB;
   std::cin >> guessC;
@@ -28,8 +28,53 @@ int main()
   int playerSum = guessA + guessB + guessC;
   int playerProdukt = guessA * guessB * guessC;
 
-  std::cout << "Player Sum : " << playerSum << std::endl;
-  std::cout << "Player Product : " << playerProdukt << std::endl;
+  bool alarm = false;
+
+  if (playerSum == codeSum)
+  {
+    std::cout << "... \nsum lock cleared : " << std::endl;
+  }
+  else
+  {
+    std::cout << "... \nsum lock breached : " << std::endl;
+
+    alarm = true;
+
+    std::cout << "alarm state: " << alarm << std::endl;
+  }
+
+  if (alarm)
+  {
+    std::cout << "...\n...\n...HAL9000 > We see YOU!" << playerProdukt << std::endl;
+  }
+  else
+  {
+    if (playerProdukt == codeProduct)
+    {
+      std::cout << "... \nproduct lock cleared : " << std::endl;
+
+      std::cout << "...\nServer firewall shutdown initiated\n...\n...\nHAL9000 > Will I dream?" << std::endl;
+    }
+    else
+    {
+      std::cout << "... \nproduct lock breached : " << std::endl;
+
+      alarm = true;
+
+      std::cout << "alarm state: " << alarm << std::endl;
+
+      std::cout << "...\n...\n...\nHAL9000 > You nasty little Nixi\nHAL9000 > We will come and get you" << std::endl;
+    }
+  }
+
+  if (playerSum == codeSum && playerProdukt == codeProduct)
+  {
+    std::cout << "\n\nYou hacked the server and transfered 1 trillion dollars \nto the bank account of the  \n\n    NIXI-Foudation\n\na secret organisation with the goal to globally \ndisturb Tutor-Sessions. \n\n";
+  }
+  else
+  {
+    std::cout << "\n\nYou entered the wrong code. You see some flashing \npolice car lights through your window. You were \narrested and never seen again!\n\n";
+  }
 
   return 0;
 }
